6135dfd1cc342b00aea34814cc70fe526fba11fd
fe-dev1 <fe-dev1@labsquire.com>
2026-03-01 19:26:53 +0530

    Update dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,41 @@
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+// import { defineConfig } from 'vite'
+// import tsconfigPaths from 'vite-tsconfig-paths'
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+// import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import viteReact from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// const config = defineConfig({
+//   plugins: [
+//     tsconfigPaths({ projects: ['./tsconfig.json'] }),
+//     tailwindcss(),
+//     tanstackStart(),
+//     viteReact(),
+//   ],
+// })
+
+// export default config
+
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 const config = defineConfig({
   plugins: [
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    devtools(),
+    nitro(),
+    viteTsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
   ],
-})
+});
 
-export default config
+export default config;
