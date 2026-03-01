765810f89dd29cb3be1099a5ea7784d2485e7b87
fe-dev1 <fe-dev1@labsquire.com>
2026-03-01 19:52:41 +0530

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,3 @@
-// import { defineConfig } from 'vite'
-// import tsconfigPaths from 'vite-tsconfig-paths'
-
-// import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-// import viteReact from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// const config = defineConfig({
-//   plugins: [
-//     tsconfigPaths({ projects: ['./tsconfig.json'] }),
-//     tailwindcss(),
-//     tanstackStart(),
-//     viteReact(),
-//   ],
-// })
-
-// export default config
-
 import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
